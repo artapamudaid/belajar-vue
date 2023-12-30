48497e3268bd0c93aae91c261211e2958ea795fc
artapamudaid <pamuda.id@gmail.com>
2023-12-30 11:37:42 +0700

    create navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
-<script setup>
-import LandingPage from '@pages/LandingPage.vue'
-</script>
-
 <template>
-  <LandingPage/>
+  <Navbar/>
 </template>
+
+<script setup>
+import Navbar from '@components/navbar/Navbar.vue'
+</script>
